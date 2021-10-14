cbe73c7f917dd383d0c3369752fce37cd6831ebc
vishal.Alok <you@example.com>
2021-10-15 04:31:14 +0530

    Add project files.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class PrimeNumberExample
+{
+    public static void Main(string[] args)
+    {
+        int number, half = 0, flag = 0;
+
+        Console.Write("Enter the Number to check Prime: ");
+        number = int.Parse(Console.ReadLine());
+
+        half = number / 2;
+
+        for (int i = 2; i <= half; i++)
+        {
+            if (number % i == 0)
+            {
+                Console.Write("Number is not Prime. ");
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            Console.Write("Number is Prime. ");
+    }
+}
